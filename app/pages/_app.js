c0303a0a2279dd98ec5f253763319bb11c8c6929
dadof3littlemonsters <dadof3littlemonsters@users.noreply.github.com>
2026-03-12 13:41:16 +0000

    Fix mobile scrolling in app shell

--- app/pages/_app.js
+++ app/pages/_app.js
@@ -93,7 +93,7 @@ export default function App({ Component, pageProps }) {
       {!isPublicPage && (
         <DesktopNav pathname={router.pathname} user={user} />
       )}
-      <div className={`${isPublicPage ? '' : 'lg:pl-64 '}h-full min-h-0`}>
+      <div className={`${isPublicPage ? '' : 'lg:pl-64 '}flex min-h-0 flex-1 flex-col`}>
         <Component {...pageProps} user={user} setUser={setUser} />
       </div>
     </>
